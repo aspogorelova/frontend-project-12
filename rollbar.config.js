58de904f63961cbc32937b5cbdf73f2a4814eb66
aspogorelova <anyapogorelova17@gmail.com>
2025-08-24 12:54:31 +0300

    fix linter roll.config.js

--- rollbar.config.js
+++ rollbar.config.js
@@ -1,9 +1,16 @@
 import Rollbar from 'rollbar'
-import process from 'node:process'
+
+const getNodeEnv = () => {
+  try {
+    return process.env.NODE_ENV || 'development';
+  } catch (error) {
+    return 'development';
+  }
+};
 
 const rollbar = new Rollbar({
   accessToken: 'a218e30b0e254c818713a532a06258711722db09c0cc4e584a6d801b00f987d4c9d74f97a9cd60f7fb638f9136991548',
-  environment: process.env.NODE_ENV || 'development',
+  environment: getNodeEnv(),
   captureUncaught: true,
   captureUnhandledRejections: true,
   payload: {
