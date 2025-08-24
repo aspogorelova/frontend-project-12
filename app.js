e24fce01709ead9249f65966eed1a5cae114a30a
aspogorelova <anyapogorelova17@gmail.com>
2025-08-24 12:44:50 +0300

    fix linter app.js roll.config.js

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-import path from 'path';
-import AutoLoad from '@fastify/autoload';
-import fastifyStatic from '@fastify/static';
+import path from 'path'
+import { fileURLToPath } from 'url'
+import AutoLoad from '@fastify/autoload'
+import fastifyStatic from '@fastify/static'
+import process from 'node:process'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default async function (fastify, opts) {
   fastify.register(fastifyStatic, {
     root: path.join(process.cwd(), 'frontend', 'dist'),
-  });
+  })
 
   fastify.setNotFoundHandler((req, reply) => {
-    reply.sendFile('index.html');
-  });
+    reply.sendFile('index.html')
+  })
 
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts),
-  });
+  })
 
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts),
-  });
+  })
 }
