720e9df9236432eadab438428e4dd600ef5a9ca4
aspogorelova <anyapogorelova17@gmail.com>
2025-08-26 19:39:30 +0300

    fix linter

--- frontend/src/middleware/unauthMiddleware.js
+++ frontend/src/middleware/unauthMiddleware.js
@@ -1,7 +1,7 @@
 import { isRejectedWithValue } from '@reduxjs/toolkit'
 import { logOut } from '../slices/authSlice.js'
 
-export const unauthenticatedMiddleware = ({ dispatch }) => next => action => {
+export const unauthenticatedMiddleware = dispatch => next => action => {
   if (isRejectedWithValue(action) && action.payload.status === 401) {
     if (!action.type.includes('authApi')) {
       dispatch(logOut())
